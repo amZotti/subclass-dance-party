8b8be512a572cb4a3b3ee2152528c674e311a640
amzotti <azotti1337@gmail.com>
2015-02-10 11:59:02 -0800

    SubClasses have their own images
    Rather than inherent a image each subclass has it's own custom
    functionality to implement images

--- src/pokeDancer.js
+++ src/pokeDancer.js
@@ -1,11 +1,24 @@
 window.PokeDancer = function(top, left, timeBetweenSteps){ 
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<img class="pokedancer" src="http://static.giantbomb.com/uploads/scale_small/0/6087/2438704-1202149925_t.png"></img>');
+  var pokemonURL = PokeDancer.prototype.getRandomPokemon();
+  this.$node = $('<img class="pokedancer" src=' + pokemonURL + '></img>');
+  console.log(this);
   this.setPosition(top, left);
 };
 
 PokeDancer.prototype = Object.create(Dancer.prototype);
 PokeDancer.prototype.constructor = PokeDancer;
+
+PokeDancer.prototype.pokemon = function() {
+  return ['http://images6.fanpop.com/image/polls/1116000/1116583_1347464724406_full.png', 'http://th00.deviantart.net/fs70/PRE/i/2012/261/2/b/pokemon_conquest___vaporeon_by_kalas17-d5f4nup.png', 'http://assets22.pokemon.com/assets/cms2/img/pokedex/full/007.png', 'http://assets22.pokemon.com/assets/cms2/img/pokedex/full/010.png'];
+};
+
+PokeDancer.prototype.getRandomPokemon = function() {
+  var pokemonURLs = PokeDancer.prototype.pokemon();
+  var randomIndex = Math.floor(Math.random() * pokemonURLs.length);
+  return pokemonURLs[randomIndex];
+};
+
 PokeDancer.prototype.setPosition = function(top, left){
   var styleSettings = {
     top: top,
