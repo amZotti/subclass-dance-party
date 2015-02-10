813b50fd16136296b609d58164fdfef40a67240a
amzotti <azotti1337@gmail.com>
2015-02-10 12:33:41 -0800

    Implement Node lineup functionality

--- src/init.js
+++ src/init.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   window.dancers = [];
 
+  var lineup = function(middleOfPage) {
+    $(".dancer, .pokedancer, .krillian").animate({ "left": middleOfPage });
+  };
+
+  $(".lineup").on("click", function(event) {
+    var middleOfPage = $("body").width() / 2;
+    lineup(middleOfPage);
+  });
+
   $(".addDancerButton").on("click", function(event){
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
     var dancerMakerFunction = window[dancerMakerFunctionName];
@@ -9,6 +18,7 @@ $(document).ready(function(){
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    window.dancers.push(dancer.$node);
     $('body').append(dancer.$node);
   });
 });
